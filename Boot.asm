487cb8511c59d764f8adb1407fb675626005c378
potato667 <87471574+potato667@users.noreply.github.com>
2023-02-08 21:00:51 +0200

    Update Boot.asm

--- Boot.asm
+++ Boot.asm
@@ -13,150 +13,145 @@
 ;         "nasm Kernel.asm -f bin -o Kernel.bin"
 ;         "type Boot.bin Kernel.bin > CruiseOS.bin"
 
-; ASM & C "gcc -c Kernel.c -o Kernel.o"
+; ASM & C "gcc -c Kernel_C.c -o Kernel_C.o -ffreestanding -no-stdlib -fno-pie -fno-pic -m32"
 ;         "nasm Kernel_Entry.asm -o Kernel_Entry.o"
-;         "ld Kernel.o Kernel_Entry.o -o elf"
+;         "ld Kernel_C.o Kernel_Entry.o -o Kernel_C_Full.o"
+;         "objcopy -O binary Kernel_C_Full.elf Kernel_C_Full.bin"
 
 ; RUN     "qemu-system-i386 CruiseOS.bin"
 ; RUN DBG "qemu-system-i386 -monitor stdio -d int -no-reboot CruiseOS.bin"
 
-; BOOTLOADER FEATURES:
-; [X]DRIVE CHECK
-; [X]HANG ROUTINES
-; [X]A20
-; [X]GDT
-; [X]32 BIT PROTECTED MODE
-; [-]64 BIT LONG MODE
-; [-]OLD BIOS PATCHES
-; [-]FAT 32 FILESYSTEM SUPPORT
-; [-]INITIALIZING THE RTC (REAL TIME CLOCK)
-; [-]INITIALIZING INT 33h (THE MOUSE CURSOR)
-; [-]IDT (INTERRUPT DESCRIPTOR TABLE)
-; [-]ISR (INTERRUPT SERVICE ROUTINE)
-; [-]VESA BIOS EXTENTIONS
-; [-]RESOLUTION TO 1920*1080
-; [-]MULTI-THREADING
+; BOOTLOADER FEATURES:-=-=-=-=-=-=-=-=-=-=-=-=
+; [X]DRIVE CHECK-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+; [X]HANG ROUTINES-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+; [X]A20-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+; [X]GDT-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+; [X]32 BIT PROTECTED MODE-=-=-=-=-=-=-=-=-=-=
+; [-]64 BIT LONG MODE -> NOT NOW-=-=-=-=-=-=-=
+; [-]OLD BIOS PATCHES=-=-=-=-=-=-=-=-=-=-=-=-=
+; [-]FAT 32 FILESYSTEM SUPPORT-=-=-=-=-=-=-=-=
+; [-]INITIALIZING THE RTC (REAL TIME CLOCK)=-=
+; [-]INITIALIZING INT 33h (THE MOUSE CURSOR)-=
+; [-]IDT (INTERRUPT DESCRIPTOR TABLE)=-=-=-=-=
+; [-]ISR (INTERRUPT SERVICE ROUTINE)-=-=-=-=-=
+; [-]VESA BIOS EXTENTIONS=-=-=-=-=-=-=-=-=-=-=
+; [-]RESOLUTION TO 1920*1080-=-=-=-=-=-=-=-=-=
+; [-]MULTI-THREADING-=-=-=-=-=-=-=-=-=-=-=-=-=
 ; [-]KERNEL ABI (APPLICATION BINARY INTERFACE)
-; [X]MOVING TO THE KERNEL
+; [X]MOVING TO THE KERNEL=-=-=-=-=-=-=-=-=-=-=
 
-; REPLACE ALL MOV REGISTER, 0 TO XOR REGISTER, REGISTER, SINCE IT IS FASTER
+[ORG 0x7C00];-=-=-=-=-=-=-=-=CODE LOCATION IE: BOOTLOADERS START IN 0x7C00-=-=-=-=-=-=-=-=
+BITS 16;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-16 BIT SEGMENT
-BITS 16 ; TO CALL OR JMP FROM ONE CODE SEGMENT TO ANOTHER, USE '0:' BEFORE THE FUNCTION NAME
-[ORG 0x7c00] ; WHERE THE CODE IS LOCATED, BOOTLOADERS ARE USUALLY IN 0x7c00
+;-=-=-=-=-=-=-=-=-=-=CLEAR CS SEGMENT REGISTER FOR A PREDICTABLE VALUE-=-=-=-=-=-=-=-=-=-=
+JMP 0:ENTRY
+ENTRY:
 
-;JMP 0:ENTRY
-;ENTRY:
+KERNEL_OFFSET EQU  0x2000 ;-=KERNEL CODE PLACEMENT / POSITION IN MEMORY IE: [ORG 0x2000]-=
+SP_OFFSET     EQU  0x7C00 ;-=-=-=BOOTLOADER CODE PLACEMENT IN MEMORY JUST LIKE ABOVE-=-=-=
+ESP_OFFSET    EQU 0x90000 ;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=???-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+AMT_OF_SECTS  EQU       1 ;HOW MANY SECTORS HAVE BEEN INITIALIZED, EACH CONTAINS 512 BYTES
 
-KERNEL_OFFSET EQU 0x2000 ; WHERE THE KERNEL WILL BE PLACED, FOR EXAMPLE [ORG 0x2000]
+;-=-=-=-=-=-=-=CLEAR THE OTHER LEFT SEGMENT REGISTERS FOR PREDICTABLE VALUES-=-=-=-=-=-=-=
+XOR AX,                AX
+MOV SS,                AX
+MOV DS,                AX
+MOV ES,                AX
+MOV GS,                AX
+MOV [BOOT_DRIVE],      DL
+MOV SP,         SP_OFFSET
 
-XOR           AX,     AX
-MOV           SS,     AX
-MOV           DS,     AX
-MOV           ES,     AX
-MOV           GS,     AX
-MOV [BOOT_DRIVE],     DL
-MOV           SP, 0x7c00
+JMP             INIT_BOOT
 
-JMP 0:INIT_KERNEL
+;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=HANG FUNCTION-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+;-=-=-=-=-=-=LEAVE A DISCRIPTIVE MESSAGE OF WHAT THE ERROR IS AND IS CAUSED BY-=-=-=-=-=-=
+HANG_ROUTINE:  CALL               OUTPUT
+        HANG:  CLI
+               HLT
+               JMP                  HANG
 
-;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-HANG ROUTINE
-HANG_ROUTINE: CALL OUTPUT; USE AFTER LEAVING A DISCRIPTIVE MESSAGE OF WHAT THE ERROR IS
-        HANG: CLI
-              HLT
-              JMP HANG
+;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=PRINT-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+OUTPUT:        MOV   AH,             0EH
+.AGAIN:        LODSB
+               CMP   AL,               0
+               JE                  .EXIT
+               INT                   10H
+               JMP                .AGAIN
+ .EXIT:        RET
 
-;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-PRINT
-OUTPUT: MOV AH, 0Eh
-.AGAIN: LODSB
-        CMP AL,   0
-        JE    .EXIT
-        INT     10h
-        JMP  .AGAIN
- .EXIT: RET
+;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-BOOTLOADING=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+INIT_BOOT:     MOV   BX,    KERNEL_OFFSET ;-=-=-=-=-=-=-=-=BX: DESTINATION-=-=-=-=-=-=-=-=
+               MOV   DH,     AMT_OF_SECTS ;-=-=-=-=-=-=-=-=DH: SECTOR NUMS-=-=-=-=-=-=-=-=
+               MOV   DL,     [BOOT_DRIVE] ;-=-=-=-=-=-=-=-=DL:    DRIVE   -=-=-=-=-=-=-=-=
+               JMP             INIT_DRIVE
 
-;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-BOOTLOADING
-INIT_KERNEL: MOV  BX, KERNEL_OFFSET ; BX: DESTINATION
-             MOV  DH,             1 ; DH: SECTOR NUMS
-             MOV  DL,  [BOOT_DRIVE] ; DL:        DISK
-             JMP        INIT_DRIVE
+INIT_DRIVE:    PUSH  DX
+               MOV   AH,              02H
+               MOV   AL,                1
+               MOV   CL,              02H
+               MOV   CH,              00H
+               MOV   DH,              00H
+               INT                    13H
+               POP                     DX
+               MOV   SI,         DISK_ERR
+               JC            HANG_ROUTINE
+               CMP   AL,               DH
+               MOV   SI,         SECT_ERR
+               JNE           HANG_ROUTINE
+               JMP              A20_CHECK
 
-INIT_DRIVE: PUSHA
-            PUSH DX
-            MOV  AH,      02h
-            MOV  AL,        1
-            MOV  CL,      02h
-            MOV  CH,      00h
-            MOV  DH,      00h
-            INT           13h
-            ; CAN I PUT POP DX HERE?
-            MOV  SI, DISK_ERR
-            JC   HANG_ROUTINE
-            POP  DX
-            CMP  AL,       DH
-            MOV  SI, SECT_ERR
-            JNE  HANG_ROUTINE
-            JMP  A20_CHECK
-
-A20_CHECK: POPA
-           IN    AL, 0x92
-           TEST  AL,    2
-           JNZ  A20_STATE
-           OR    AL,    2
-           AND   AL, 0xFE
-           OUT 0x92,   AL
+A20_CHECK:     IN    AL,              92H
+               TEST  AL,                2
+               JNZ          BITS32_SWITCH
+               OR    AL,                2
+               AND   AL,             0xFE
+               OUT  92H,               AL
 
 BITS32_SWITCH: CLI
-               LGDT [GDT_TABLE]
-               MOV  EAX,    CR0
-               OR   EAX,     1h
-               MOV  CR0,    EAX
-               MOV   AX,     16
-               MOV   DS,     AX
-               MOV   SS,     AX
-               MOV   ES,     AX
-               MOV   FS,     AX
-               MOV   GS,     AX
-               JMP  8:INIT_32BITS
+               LGDT           [GDT_TABLE]
+               MOV   EAX,             CR0
+               OR    EAX,              1H
+               MOV   CR0,             EAX
+               MOV    AX,              16
+               MOV    DS,              AX
+               MOV    SS,              AX
+               MOV    ES,              AX
+               MOV    FS,              AX
+               MOV    GS,              AX
+               JMP          8:INIT_32BITS
 
-A20_STATE: IN   AL, 0x92
-           TEST AL,    2
-           JNZ  BITS32_SWITCH
-           MOV  SI, A20_STATE_OFF
-           JMP  HANG_ROUTINE
+BITS 32;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-32 BIT SEGMENT
-BITS 32
+INIT_32BITS:   MOV   ESP,      ESP_OFFSET
+               JMP          KERNEL_OFFSET
 
-INIT_32BITS: MOV  EBP, 90000h
-             MOV  ESP,    EBP
-             JMP  BEGIN_32BIT
+;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=VARIABLES-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+BOOT_DRIVE     DB                       0
+DISK_ERR       DB          'DISK_ERR ', 0
+SECT_ERR       DB          'SECT_ERR ', 0
+A20_STATE_OFF  DB           'A20_OFF ', 0
+BOOT_ERR       DB          'BOOT_ERR ', 0
+GDT_START      DQ                      0H
+GDT_CODE       DW                  0xFFFF
+               DW                      0H
+               DB                      0H
+               DB               10011010B
+               DB               11001111B
+               DB                      0H
+GDT_DATA       DW                  0xFFFF
+               DW                      0H
+               DB                      0H
+               DB               10010010B
+               DB               11001111B
+               DB                      0H
+GDT_TABLE      DW GDT_END - GDT_START - 1
+               DD               GDT_START
+GDT_END        DB                       0
+;JOKE          DB 'FIND HOT SINGLES AT HTTPS:',0
+;MOV SI, JOKE       ; ISSUE HERE
+;CALL HANG_ROUTINE
 
-BEGIN_32BIT: CALL KERNEL_OFFSET
-             JMP $
-
-;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-VARIABLES
-BOOT_DRIVE    DB 0
-DISK_ERR      DB 'DISK_ERR ', 0
-SECT_ERR      DB 'SECT_ERR ', 0
-A20_STATE_OFF DB 'A20_OFF ' , 0
-BOOT_ERR      DB 'BOOT_ERR ', 0
-GDT_TABLE:    DW GDT_END - GDT_START - 1
-              DD GDT_START
-GDT_START:    DQ       0x0
-GDT_CODE:     DW    0xffff
-              DW       0x0
-              DB       0x0
-              DB 10011010b
-              DB 11001111b
-              DB       0x0
-GDT_DATA:     DW    0xffff
-              DW       0x0
-              DB       0x0
-              DB 10010010b
-              DB 11001111b
-              DB       0x0
-GDT_END:
-
-TIMES 510-($-$$) DB 0
-DW 0xAA55 ; BOOT SIGNATURE
+;-=-=-=-=-=-=-=-=-=-=-=-=PAD OUT THE REST OF THE BOOTLOADER MEMORY-=-=-=-=-=-=-=-=-=-=-=-=
+TIMES 510 - ($ - $$) DB 0
+;-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=BOOT SIGN-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+DW 0xAA55
