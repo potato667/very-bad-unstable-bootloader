1981a149688aaac47833e5c6d535b6906e590e73
potato667 <87471574+potato667@users.noreply.github.com>
2023-09-11 21:22:29 +0300

    Create Interrupt_test.asm

---
+++ Interrupt_test.asm
@@ -0,0 +1,16 @@
+BITS 64
+
+START_64BITS:LIDT[IDT64_TABLE]
+             JMP $
+
+IDT64_START: DW             IDT64_ISR & 0FFFFH
+             DW                     GDT64_CODE
+             DB                            00H
+             DB                    IDT64_FLAGS
+             DW (IDT64_ISR >> 16) &     0FFFFH
+             DD (IDT64_ISR >> 32) & 0FFFFFFFFH
+             DD                            00H
+IDT64_TABLE: DW            $ - IDT64_START - 1
+             DQ                    IDT64_START
+IDT64_FLAGS: DB                            00H
+IDT64_ISR:   DB                            00H
